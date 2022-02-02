a820b0d0bf46d9d9c1abe239468caf57d462ef3d
Michał Paks <mpaks@incuvo.com>
2022-02-02 20:23:55 +0100

    Gun and pickup item

---
+++ ZaliczenieMag/Assets/Pickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject gun;
+    public GameObject mesh;
+    public Transform meshT;
+    private float rotateFloat = 0f;
+    public AudioClip soundB;
+
+    void Start()
+    {
+        //gun = GameObject.FindWithTag("GunA");
+        meshT = mesh.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+
+        meshT.rotation = Quaternion.Euler(0, rotateFloat, 0);
+        rotateFloat += 30f * Time.deltaTime;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(soundB, gameObject.transform.position);
+        gun.SetActive(true);
+        Destroy(gameObject);
+    }
+}
